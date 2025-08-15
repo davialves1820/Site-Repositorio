8e7a07d5bc3dd343ee2fe1a3816275f84041aa9a
Davi Alves <davialvesr18@gmail.com>
2025-08-15 10:22:01 -0300

    Validacao e verificacao se o email existe.

--- backend/index.js
+++ backend/index.js
@@ -17,21 +17,52 @@ const client = new google.auth.JWT({
 
 const spreadsheetId = "1LVh2ApF6tCoj9wkAOIgPlOCg7MuRhY3p7e6HXgOFnK0"; // substitua pelo ID da sua planilha
 
+// Função para verificar se o email é válido
+function is_valid_email(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return re.test(email);
+}
+
+// Função para verificar se o email já está na planilha
+async function email_exist(email, sheets) {
+    const re = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range: "B:B"
+    });
+
+    const emails = res.data.values ? res.data.values.flat() : [];
+    
+    return emails.includes(email);
+}
+
 // Rota para receber e-mails
 app.post("/add-email", async (req, res) => {
     console.log("Body recebido:", req.body);
+    
     const { email, nome } = req.body;
+    
+    if (!is_valid_email(email)) {
+        return res.status(400).json({ status: "error", message: "Email inválido."});
+    }
+
     try {
         await client.authorize();
         const sheets = google.sheets({ version: "v4", auth: client });
+
+        if (email_exist(email, sheets)) {
+            return res.status(400),json({ status: "error", message: "Email já existente."});
+        }
+
         await sheets.spreadsheets.values.append({
-        spreadsheetId,
-        range: "A:C",
-        valueInputOption: "RAW",
-        resource: {
-            values: [[email, nome, new Date().toISOString()]],
-        },
+            spreadsheetId,
+            range: "A:C",
+            valueInputOption: "RAW",
+            resource: {
+                values: [[email, nome, new Date().toISOString()]],
+            },
         });
+        
         res.json({ status: "success", message: "Email salvo com sucesso!" });
     } catch (err) {
         console.error(err);
