4c7673ccd9615d75477a32e94b58943a5f7faafb
Davi Alves <davialvesr18@gmail.com>
2025-08-21 09:11:08 -0300

    Comentarios

--- src/components/project_card.jsx
+++ src/components/project_card.jsx
@@ -1,5 +1,6 @@
 import { Col } from "react-bootstrap";
 
+// Modelo de estrutura de um projeto
 export const ProjectCard = ({ title, description, img_url}) => {
     return (
         <Col size={12} sm={6} md={4}>
