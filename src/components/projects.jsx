ba0e5a547a37e313ee551610cb538481c090c459
Davi Alves <davialvesr18@gmail.com>
2025-08-20 21:47:43 -0300

    Projects

---
+++ src/components/projects.jsx
@@ -0,0 +1,43 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./project_card";
+import TrackVisibility from "react-on-screen";
+import "animate.css";
+
+export const Project = () => {
+    const projects = [
+        {
+            title: "Otimization functions application",
+            description: "Otimization algorotihms in a real problem",
+            img_url: "",
+        }
+    ];
+
+    return (
+        <section className="project" id="projects">
+            <Container>
+                <Row>
+                    <Col size={12}>
+                        <TrackVisibility>
+                            {({is_visivble}) =>
+                                <div className={is_visivble ? "animate__animated animated__fadeIn" : ""}>
+                                    <h2>Projects</h2>
+
+                                    <p>jvanjnfnvjasnjawrfbsfhkbfshkvbafhbfkaufoabvsjhfskjhouwhuofbewobcoabncowhaouh</p>
+
+                                    <Tab.Container id="projects-tab" defaultActiveKey="first">
+                                        <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                            <Nav.Item>
+                                                <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                                            </Nav.Item>
+                                        </Nav>
+                                        
+                                    </Tab.Container>
+                                </div>
+                            } 
+                        </TrackVisibility>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
