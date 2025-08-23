8fa95d857d3280a79e6d0249d348d3afe55832b0
Davi Alves <davialvesr18@gmail.com>
2025-08-23 14:08:05 -0300

    Projects

--- src/components/projects/project_card.jsx
+++ src/components/projects/project_card.jsx
@@ -2,17 +2,17 @@ import { Col } from "react-bootstrap";
 import "../projects/projects.css";
 
 export const ProjectCard = ({ title, description, imgUrl: img_url }) => {
-  return (
-    <Col xs={12} sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={img_url} alt={title} />
+    return (
+        <Col xs={12} sm={6} md={4}>
+        <div className="proj-imgbx">
+            <img src={img_url} alt={title} />
 
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
+            <div className="proj-txtx">
+            <h4>{title}</h4>
+            <span>{description}</span>
+            </div>
         </div>
-      </div>
-    </Col>
-  );
+        </Col>
+    );
 };
 
